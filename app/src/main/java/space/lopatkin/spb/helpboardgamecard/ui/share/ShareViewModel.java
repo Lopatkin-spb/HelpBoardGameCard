2c0778375ae8f84e1091cdf003e362487fc300b4
Lopatkin-spb <lopatkin.spb@mail.ru>
2020-08-21 02:14:26 +0400

    удалены лишние фрагменты и остальные переименованы

--- app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/share/ShareViewModel.java
+++ app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/share/ShareViewModel.java
@@ -10,7 +10,7 @@ public class ShareViewModel extends ViewModel {
 
     public ShareViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is share fragment");
+        mText.setValue("This is share fragment. пока свободный фрагмент");
     }
 
     public LiveData<String> getText() {
