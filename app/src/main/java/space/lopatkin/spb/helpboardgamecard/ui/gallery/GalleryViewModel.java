dd3e9cce723eddbb54405c3d3147004517cfce3c
Lopatkin-spb <lopatkin.spb@mail.ru>
2020-08-20 19:11:15 +0400

    создан проект на основе DrawerActivity (выложены файлы кроме androidTest, test, HelpBoardGameCard)

---
+++ app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package space.lopatkin.spb.helpboardgamecard.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
