cf9f457f78ab3ff13eaeb25960fba73dca645ba4
Lopatkin-spb <lopatkin.spb@mail.ru>
2020-08-21 21:36:11 +0400

    создан интерфейс HelpCard Dao

---
+++ app/src/main/java/space/lopatkin/spb/helpboardgamecard/data/HelpCardDao.java
@@ -0,0 +1,43 @@
+package space.lopatkin.spb.helpboardgamecard.data;
+
+
+import androidx.lifecycle.LiveData;
+import androidx.room.*;
+import space.lopatkin.spb.helpboardgamecard.model.HelpCard;
+
+import java.util.List;
+
+@Dao
+public interface HelpCardDao {
+
+    //выбрать все
+    @Query("SELECT * FROM helpCard")
+    List<HelpCard> getAll();
+
+    //обновление списка с данными через ливдата (недопонял)
+    @Query("SELECT * FROM helpCard")
+    LiveData<List<HelpCard>> getAllLiveData();
+
+    //
+    @Query("SELECT * FROM helpCard WHERE uniqueId IN (:userIds)")
+    List<HelpCard> loadAllByIds(int[] userIds);
+
+    //
+    @Query("SELECT * FROM helpCard WHERE uniqueId = :uniqueId LIMIT 1")
+    HelpCard findById(int uniqueId);
+
+    //если при вставке сущности уже есть с таким названием
+    // - сущность заменяется на новую
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(HelpCard helpCard);
+
+    //функция для обновления
+    @Update
+    void update(HelpCard helpCard);
+
+    //
+    @Delete
+    void delete(HelpCard helpCard);
+
+
+}
