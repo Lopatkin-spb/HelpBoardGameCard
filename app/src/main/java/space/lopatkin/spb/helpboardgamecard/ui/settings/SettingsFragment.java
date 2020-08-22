800ce22f3037f1d1f2f6966dd33c331a860905f0
Lopatkin-spb <lopatkin.spb@mail.ru>
2020-08-22 16:55:40 +0400

    создана ветка тестовая (чтото сделано но с ошибками)

--- app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/settings/SettingsFragment.java
+++ app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/settings/SettingsFragment.java
@@ -21,7 +21,13 @@ public class SettingsFragment extends Fragment {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+
         final TextView textView = root.findViewById(R.id.text_settings);
+
+
+
         settingsViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
