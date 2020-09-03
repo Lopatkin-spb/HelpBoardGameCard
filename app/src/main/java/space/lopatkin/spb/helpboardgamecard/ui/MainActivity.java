8ec3c6f1fa146c222fdc440f7359495363687bc3
Lopatkin-spb <lopatkin.spb@mail.ru>
2020-09-04 01:21:50 +0400

    ХМЛ дровера переименованы и подкорректированы по мелочи

--- app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/MainActivity.java
+++ app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_activity_main);
 //        тулбар
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
