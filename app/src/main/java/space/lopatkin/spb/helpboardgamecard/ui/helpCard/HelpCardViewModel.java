9717c2008ef9f946014e23c07c5966db9ec6c705
Lopatkin-spb <lopatkin.spb@mail.ru>
2023-07-29 15:28:10 +0400

    - HelpCard: fix errors in code;

--- app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/helpCard/HelpCardViewModel.java
+++ app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/helpCard/HelpCardViewModel.java
@@ -1,40 +1,19 @@
-//package space.lopatkin.spb.helpboardgamecard.ui.helpCard;
-//
-//import androidx.lifecycle.LiveData;
-//import androidx.lifecycle.MutableLiveData;
-//import androidx.lifecycle.ViewModel;
-//import androidx.recyclerview.widget.RecyclerView;
-//import space.lopatkin.spb.helpboardgamecard.R;
-//
-//public class HelpCardViewModel extends ViewModel {
-//
+package space.lopatkin.spb.helpboardgamecard.ui.helpcard;
 
-//непонятное!!!!!!
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
+public class HelpCardViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
 
-//    private MutableLiveData<String> mText;
-//
-//
-//    private RecyclerView recyclerView;
-//
-//
-//    public HelpCardViewModel() {
-//        mText = new MutableLiveData<>();
-//        mText.setValue("тут хранится каталог памяток");
-//
-//        //список
-//        recyclerView = findViewById(R.id.list);
-//
-//
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager (this, RecyclerView.VERTICAL, false);
-//        recyclerView.setLayoutManager(linearLayoutManager);
-//        //разделитель
-//        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-//
-//
-//    }
-//
-//    public LiveData<String> getText() {
-//        return mText;
-//    }
-//}
+    public HelpCardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("тут хранится каталог памяток");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+}
--- app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/helpcard/HelpcardFragment.java
+++ app/src/main/java/space/lopatkin/spb/helpboardgamecard/ui/helpcard/HelpcardFragment.java
@@ -5,175 +5,53 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import space.lopatkin.spb.helpboardgamecard.R;
-import space.lopatkin.spb.helpboardgamecard.model.Helpcard;
+import space.lopatkin.spb.helpboardgamecard.ui.helpcard.HelpCardViewModel;
 
+public class HelpCardFragment extends Fragment {
 
-public class HelpcardFragment extends Fragment {
+    private HelpCardViewModel helpCardViewModel;
 
-    public static final String EXTRA_TITLE =
-            "space.lopatkin.spb.testnavdrawer.ui.add.EXTRA_TITLE";
-    public static final String EXTRA_DESCRIPTION =
-            "space.lopatkin.spb.testnavdrawer.ui.add.EXTRA_DESCRIPTION";
-    public static final String EXTRA_PRIORITY =
-            "space.lopatkin.spb.testnavdrawer.ui.add.EXTRA_PRIORITY";
+    private RecyclerView recyclerView;
 
-    private TextView textViewTitle;
-    private TextView textViewVictoryCondition;
-    private TextView textViewEndGame;
-    private TextView textViewPreparation;
-    private TextView textViewDescription;
-    private TextView textViewPlayerTurn;
-    private TextView textViewEffects;
-    //    private boolean editCheckFavorites;
-    private TextView textViewPriority;
-
-
-    NavController navController;
-
-//    //включение режим вывода елементов фрагмента в action bar
-//    @Override
-//    public void onCreate(@Nullable Bundle savedInstanceState) {
-//        setHasOptionsMenu(true);
-//        super.onCreate(savedInstanceState);
-//    }
-
-
-    //создание вью
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        helpCardViewModel =
+                ViewModelProviders.of(this).get(HelpCardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_helpcard, container, false);
 
 
-        //инициализация полей вью
-//        textViewTitle = root.findViewById(R.id.text_view_title);
-//        textViewDescription = root.findViewById(R.id.text_view_description);
-
-        textViewVictoryCondition = root.findViewById(R.id.text_view_victory_condition);
-        textViewEndGame = root.findViewById(R.id.text_view_end_game);
-        textViewPreparation = root.findViewById(R.id.text_view_preparation);
-        textViewPlayerTurn = root.findViewById(R.id.text_view_player_turn);
-        textViewEffects = root.findViewById(R.id.text_view_effects);
-//        editCheckFavorites = false;
-        textViewPriority = root.findViewById(R.id.text_view_priority);
 
 
-        //устанавливает в верхнем меню левую иконку
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);
 
-//        //устанавливает тайтл динамически
-//        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.menu_view_card);
+//        //список
+//        recyclerView = root.findViewById(R.id.list);
+//
+//
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager (this, RecyclerView.VERTICAL, false);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//        //разделитель
+//        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
-//        //разрешает верхнее правое меню
-//        setHasOptionsMenu(true);
+
+
+
+
 
 
         return root;
     }
 
+    private void init() {
 
-    //понадобится для сейва в будущем
-//    private void saveHelpcard() {
-//        //забор данных в переменные
-//        String messageTitle = editTextTitle.getText().toString();
-//        String messageVictoryCondition = editTextVictoryCondition.getText().toString();
-//        String messageEndGame = editTextEndGame.getText().toString();
-//        String messagePreparation = editTextPreparation.getText().toString();
-//        String messageDescription = editTextDescription.getText().toString();
-//
-//        String messagePlayerTurn = editTextPlayerTurn.getText().toString();
-//        String messageEffects = editTextEffects.getText().toString();
-//
-//        boolean messageFavorites = editCheckFavorites;
-//        int messagePriority = numberPickerPriority.getValue();
-//
-////        if (messageTitle.trim().isEmpty() || messageDescription.trim().isEmpty()) {
-//        if (messageTitle.trim().isEmpty()) {
-//            Toast.makeText(getActivity(), "Please insert a title", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-////        //интент для обычного передачи результата
-////        Intent data = new Intent();
-////        data.putExtra(EXTRA_TITLE, title);
-////        data.putExtra(EXTRA_DESCRIPTION, description);
-////        data.putExtra(EXTRA_PRIORITY, priority);
-////        getActivity().setResult(RESULT_OK, data);
-////
-////        Toast.makeText(getActivity(), "" + data, Toast.LENGTH_LONG).show();
-////        //vstavit navigatiu
-//
-//
-//        //v5safeargs
-//        AddcardEditcardFragmentDirections.ActionNavAddcardToNavHelpcard action =
-//                AddcardEditcardFragmentDirections.actionNavAddcardToNavHelpcard();
-//        action.setMessageTitle(messageTitle);
-//
-//        action.setMessageVictoryCondition(messageVictoryCondition);
-//        action.setMessageEndGame(messageEndGame);
-//        action.setMessagePreparation(messagePreparation);
-//
-//        action.setMessageDescription(messageDescription);
-//
-//        action.setMessagePlayerTurn(messagePlayerTurn);
-//        action.setMessageEffects(messageEffects);
-//
-//
-//        action.setMessageFavorites(messageFavorites);
-//        action.setMessagePriority(messagePriority);
-//
-//        //если перепись заметки то отправляется старый АйДи
-//        AddcardEditcardFragmentArgs args = AddcardEditcardFragmentArgs.fromBundle(getArguments());
-//        int id = args.getMessageId();
-//        if (id != -1) {
-//            action.setMessageId(id);
-//        }
-//
-//        navController.navigate(action);
-//    }
-//
-
-
-    //safeargs + parcelable
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        navController = Navigation.findNavController(getView());
-        if (getArguments() != null) {
-            HelpcardFragmentArgs args = HelpcardFragmentArgs.fromBundle(getArguments());
-            Helpcard messageHelpcard = args.getHelpcard();
-            if (messageHelpcard != null) {
-                //установка значений в поля
-//            textViewTitle.setText(args.getMessageTitle());
-                //            textViewDescription.setText(args.getMessageDescription());
-
-                textViewVictoryCondition.setText(messageHelpcard.getVictoryCondition());
-                textViewEndGame.setText(messageHelpcard.getEndGame());
-                textViewPreparation.setText(messageHelpcard.getPreparation());
-                textViewPlayerTurn.setText(messageHelpcard.getPlayerTurn());
-                textViewEffects.setText(messageHelpcard.getEffects());
-//                t = args.getMessageFavorites();
-                textViewPriority.setText(String.valueOf(messageHelpcard.getPriority()));
-
-                String titleUp = messageHelpcard.getTitle();
-                String descriptionUp = messageHelpcard.getDescription();
-                //устанавливает тайтл динамически
-                ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(descriptionUp + " " + titleUp);
-            } else {
-                //устанавливает тайтл динамически
-                ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.menu_view_card);
-            }
-        }
     }
 
-
 }
-
-
