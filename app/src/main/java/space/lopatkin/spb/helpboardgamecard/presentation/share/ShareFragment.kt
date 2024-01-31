bc35c89949a0cd71fc5166337ec7dd9e64d9359e
Lopatkin-spb <lopatkin.spb@mail.ru>
2024-01-31 18:34:59 +0400

    - Gradle: deleted redundant dependencies;

--- app/src/main/java/space/lopatkin/spb/helpboardgamecard/presentation/share/ShareFragment.kt
+++ app/src/main/java/space/lopatkin/spb/helpboardgamecard/presentation/share/ShareFragment.kt
@@ -6,20 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import space.lopatkin.spb.helpboardgamecard.R
 
 class ShareFragment : Fragment() {
 
-    private var shareViewModel: ShareViewModel? = null
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        shareViewModel = ViewModelProviders.of(this).get(ShareViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_share, container, false)
-        val textView = root.findViewById<TextView>(R.id.text_share)
-        shareViewModel!!.text.observe(viewLifecycleOwner) { s -> textView.text = s }
-        return root
-    }
+//    private var shareViewModel: ShareViewModel? = null
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?, savedInstanceState: Bundle?
+//    ): View? {
+//        shareViewModel = ViewModelProviders.of(this).get(ShareViewModel::class.java)
+//        val root = inflater.inflate(R.layout.fragment_share, container, false)
+//        val textView = root.findViewById<TextView>(R.id.text_share)
+//        shareViewModel!!.text.observe(viewLifecycleOwner) { s -> textView.text = s }
+//        return root
+//    }
 }
